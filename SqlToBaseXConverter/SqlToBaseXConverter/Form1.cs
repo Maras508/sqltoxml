215d5eb1e0b8d7cdbb7871b78cbbaa9e9716c8fa
marek <marek@MAREK-KOMPUTER>
2017-03-29 07:23:47 +0200

    v6

--- SqlToBaseXConverter/SqlToBaseXConverter/Form1.cs
+++ SqlToBaseXConverter/SqlToBaseXConverter/Form1.cs
@@ -20,58 +20,13 @@ namespace SqlToBaseXConverter
             InitializeComponent();
         }
 
-        private SqlConnector sqlConnector;
         private DatabaseHelper databaseHelper;
 
 
         private void button1_Click(object sender, EventArgs e)
         {
             
-            string nazwaBazy = "AdventureWorksDW2008R2";
-            SqlConnector sql = new SqlConnector();
-            sql.Connect(nazwaBazy);
-
-            DatabaseHelper dh = new DatabaseHelper(sql);
-            dh.GetTablesInfo();
-            BaseXConnector basexConnector = new BaseXConnector();
-            basexConnector.Connect();
             
-            
-            // MemoryStream ms = new MemoryStream(
-            // System.Text.Encoding.UTF8.GetBytes("<" + nazwaBazy + "></" + nazwaBazy + ">"));
-
-            // create database
-            //*************************
-            /*
-            basexConnector.session.Create(nazwaBazy, ms);
-            basexConnector.Connect(nazwaBazy);
-            Converter converter = new Converter(basexConnector, sql, nazwaBazy, dh);
-            converter.SqlToXml();
-            */
-            //*********************************************************
-           // Converter converter = new Converter(basexConnector, sql, nazwaBazy, dh);
-           // converter.SqlToXml();
-
-            //Console.WriteLine("To jest to!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            //Console.WriteLine(dh.tablesInfo["Towary"].Item1);
-            /*
-            SqlCommand command = new SqlCommand("Select * From Towary", sql.connection);
-           
-            sql.connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-            try
-            {
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader["Marka"]);// etc
-                }
-            }
-            finally
-            {
-                // Always call Close when done reading.
-                reader.Close();
-            }
-            */
         }
 
         private void bt_loadDatabase_Click(object sender, EventArgs e)
@@ -80,10 +35,10 @@ namespace SqlToBaseXConverter
 
             if (nazwaBazy != "")
             {
-                sqlConnector = SqlConnector.GetConnector();
-                sqlConnector.Connect(nazwaBazy);
+                //sqlConnector = SqlConnector.GetConnector();
+                SqlConnector.GetConnector().Connect(nazwaBazy);
 
-                databaseHelper = new DatabaseHelper(sqlConnector);
+                databaseHelper = new DatabaseHelper(SqlConnector.GetConnector());
                 databaseHelper.GetTablesInfo();
 
                 lb_quantityOfTables.Text = databaseHelper.tablesInfo.Count().ToString();
@@ -119,13 +74,12 @@ namespace SqlToBaseXConverter
         {
             if(rb_sqlToBasex.Checked)
             {
-                BaseXConnector basexConnector = new BaseXConnector();
-                basexConnector.Connect();
+                BaseXConnector.GetConnector().Connect();
 
                 
                 //setActualTable("None");
                 
-                Converter converter = new BaseXConverter(basexConnector, sqlConnector, tx_databaseName.Text, databaseHelper, this);
+                Converter converter = new BaseXConverter(BaseXConnector.GetConnector(), SqlConnector.GetConnector(), tx_databaseName.Text, databaseHelper, this);
 
                 if (rb_modeSemiAutomatic.Checked)
                 {
